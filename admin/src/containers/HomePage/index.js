076c73ed050824294e2dd2ea4bf308624ec46b96
Jefferson Botelho <jgbotelho3@outlook.com>
2020-08-15 15:27:11 -0300

    Change Home Page

---
+++ admin/src/containers/HomePage/index.js
@@ -0,0 +1,101 @@
+/*
+ *
+ * HomePage
+ *
+ */
+/* eslint-disable */
+import React, { memo } from 'react';
+import { FormattedMessage } from 'react-intl';
+import { get } from 'lodash';
+
+import PageTitle from '../../components/PageTitle';
+
+import useFetch from './hooks';
+import { ALink, Block, Container, LinkWrapper, P, Separator } from './components';
+import SocialLink from './SocialLink';
+
+const FIRST_BLOCK_LINKS = [
+  {
+    link:
+      'https://strapi.io/documentation/v3.x/getting-started/quick-start.html#_4-create-a-category-content-type',
+    contentId: 'app.components.BlockLink.documentation.content',
+    titleId: 'app.components.BlockLink.documentation',
+  },
+  {
+    link: 'https://github.com/strapi/foodadvisor',
+    contentId: 'app.components.BlockLink.code.content',
+    titleId: 'app.components.BlockLink.code',
+  },
+];
+
+const SOCIAL_LINKS = [
+  {
+    name: 'GitHub',
+    link: 'https://github.com/jgbotelho3/won-games-api',
+  },
+
+  {
+    name: 'Twitter',
+    link: 'https://twitter.com/jgbotelho3',
+  },
+];
+
+const HomePage = () => {
+  return (
+    <>
+      <FormattedMessage id="HomePage.helmet.title">
+        {title => <PageTitle title={title} />}
+      </FormattedMessage>
+      <Container className="container-fluid">
+        <div className="row">
+          <div className="col-lg-8 col-md-12">
+            <Block>
+            <h2 id="mainHeader">Bem vindo a Won Games</h2>
+              <P>Conheça nossa plataforma de games e desfrute dos melhores momentos</P>
+              <Separator style={{ marginTop: 37, marginBottom: 36 }} />
+              <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                {FIRST_BLOCK_LINKS.map((data, index) => {
+                  const type = index === 0 ? 'doc' : 'code';
+
+                  return (
+                    <LinkWrapper href={data.link} target="_blank" key={data.link} type={type}>
+                      <FormattedMessage id={data.titleId}>
+                        {title => <p className="bold">{title}</p>}
+                      </FormattedMessage>
+                      <FormattedMessage id={data.contentId}>
+                        {content => <p>{content}</p>}
+                      </FormattedMessage>
+                    </LinkWrapper>
+                  );
+                })}
+              </div>
+            </Block>
+          </div>
+
+          <div className="col-md-12 col-lg-4">
+            <Block style={{ paddingRight: 30, paddingBottom: 0 }}>
+            <h2>Entre em contato comigo</h2>
+            <P style={{ marginTop: 7, marginBottom: 0 }}>Em caso de dúvidas entre em contato comigo pelas redes sociais</P>
+              <Separator style={{ marginTop: 18 }} />
+              <div
+                className="row social-wrapper"
+                style={{
+                  display: 'flex',
+                  margin: 0,
+                  marginTop: 36,
+                  marginLeft: -15,
+                }}
+              >
+                {SOCIAL_LINKS.map((value, key) => (
+                  <SocialLink key={key} {...value} />
+                ))}
+              </div>
+            </Block>
+          </div>
+        </div>
+      </Container>
+    </>
+  );
+};
+
+export default memo(HomePage);
