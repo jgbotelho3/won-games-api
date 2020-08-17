891c2889cd763234abbd999af0d5ce2dff9274bd
Jefferson Botelho <jgbotelho3@outlook.com>
2020-08-17 15:50:33 -0300

    Config local plugin

--- plugins/repositories/admin/src/containers/HomePage/index.js
+++ plugins/repositories/admin/src/containers/HomePage/index.js
@@ -1,19 +1,47 @@
-/*
- *
- * HomePage
- *
- */
+import React, { useState, useEffect, memo } from 'react';
+import { Header } from '@buffetjs/custom';
+import { Table } from '@buffetjs/core';
+import styled from 'styled-components';
+import axios from 'axios';
 
-import React, { memo } from 'react';
-// import PropTypes from 'prop-types';
-import pluginId from '../../pluginId';
+
+
+const Wrapper = styled.div`
+  padding: 18px 30px;
+  p{
+    margin-top: 1rem;
+  }
+`
 
 const HomePage = () => {
+  const [rows, setRows] = useState([])
+
+  useEffect(() => {
+    axios.get("https://api.github.com/users/jgbotelho3/repos")
+    .then((res) => setRows(res.data))
+    .catch(e => createStrapi.notification.error(`Github API error, ${e}`))
+  })
+  const headers = [
+    {
+      name: "Name",
+      value: "name"
+    },
+    {
+      name: "url",
+      value: "html_url"
+    },
+  ];
+
   return (
-    <div>
-      <h1>{pluginId}&apos;s HomePage</h1>
-      <p>Happy coding</p>
-    </div>
+    
+    <Wrapper>
+      <Header
+      title={{ label: 'Won Games Repositories' }}
+      content="A list of Repositories from won Games"
+      />
+      <Table headers={headers} rows={rows}/>
+    </Wrapper>
+    
   );
 };
 
