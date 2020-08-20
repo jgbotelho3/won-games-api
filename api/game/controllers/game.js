28be1d476c020c446e0fb885f748f6270287cb80
Jefferson Botelho <jgbotelho3@outlook.com>
2020-08-20 15:14:08 -0300

    Fetch to Get Game Info

--- api/game/controllers/game.js
+++ api/game/controllers/game.js
@@ -1,8 +1,16 @@
 'use strict';
 
+const { default: createStrapi } = require("strapi");
+
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  populate: async (ctx) =>{
+    console.log("Start to populate...")
+    await strapi.services.game.populate()
+    ctx.send("Finished populating")
+  }
+};
--- api/game/services/game.js
+++ api/game/services/game.js
@@ -5,4 +5,38 @@
  * to customize this service
  */
 
-module.exports = {};
+const axios = require("axios");
+
+async function getGameInfo(slug) {
+  const jsdom = require("jsdom");
+  const { JSDOM } = jsdom;
+  const body = await axios.get(`https://www.gog.com/game/${slug}`);
+  const dom = new JSDOM(body.data);
+
+  const ratingElement = dom.window.document.querySelector(".age-restrictions__icon use");
+  const rating = ratingElement ? ratingElement.getAttribute("xlink:href")
+  .replace(/_/g, "")
+  .replace(/[^\w-]+/g, "") : "Free";
+
+  const description = dom.window.document.querySelector(".description");
+
+  return {
+    rating,
+    short_description: description.textContent.trim().slice(0, 160),
+    description: description.innerHTML,
+  }
+
+
+}
+
+module.exports = {
+  populate: async (params) => {
+    const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`
+
+    const { data: { products } } = await axios.get(gogApiUrl)
+
+    // console.log(products[2].slug);
+
+   console.log(await getGameInfo(products[2].slug));
+  }
+};
